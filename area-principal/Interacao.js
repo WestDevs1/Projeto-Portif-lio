852b96005ae2f889af43fd0fa421e474abebe25d
mariamillenafarias10-ops <mariamillenafarias10@gmail.com>
2025-11-23 00:19:34 -0300

    Deixando o site funcional

---
+++ area-principal/Interacao.js
@@ -0,0 +1,16 @@
+document.getElementById("btnEntrar").addEventListener("click", function() {
+    window.open("../aba-login/login.html", "_blank");
+});
+
+
+document.getElementById("criarBotao").addEventListener("click", function() {
+    window.open("../aba-criarPortifolio/criarPortifolio.html", "_blank");
+});
+
+document.getElementById("explorarBotao").addEventListener("click", function() {
+    window.open("../aba-explorarBarb/explorarbarb.html", "_blank");
+});
+
+document.getElementById("comecarAgora").addEventListener("click", function() {
+    window.open("../aba-criarPortifolio/criarPortifolio.html", "_blank"); window.history.back();
+});

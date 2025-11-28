fc09b0176f4fa89923b4e46808b5758839a10370
Viniciusxz <viniciusribeirolinsr@gmail.com>
2025-11-28 18:46:55 -0300

    Editando algumas paginas HTML e CSS e adicionando ultima aba do site

--- aba-explorarBarb/explorarbarb-interacao.js
+++ aba-explorarBarb/explorarbarb-interacao.js
@@ -1,3 +1,11 @@
-document.getElementById("explorarBotao").addEventListener("click", function() {
-    window.open("../aba-explorarBarb/explorarbarb.html", "_self");
+document.getElementById("btn1").addEventListener("click", () => {
+    window.location.href = "../aba-perfil-barbeiro/perfil-barbeiro.html";
+});
+
+document.getElementById("btn2").addEventListener("click", () => {
+    window.location.href = "../aba-perfil-barbeiro/perfil-barbeiro2.html";
+});
+
+document.getElementById("btn3").addEventListener("click", () => {
+    window.location.href = "../aba-perfil-barbeiro/perfil-barbeiro3.html";
 });
